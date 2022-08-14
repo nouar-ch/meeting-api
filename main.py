d5e71236fba2018f28f2057c7e96964477cfa23a
Nouar Chabane <nouar.chabane.dev@gmail.com>
2022-08-14 18:56:03 +0100

    setup api endpoint to retrieve post request with requested dates.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from flask import Flask, request, jsonify
+
+APP = Flask(__name__)
+
+def calculate_best_dates(dates):
+    return []
+
+@APP.post('/api/v1/calculate-best-dates')
+def best_dates():
+    requested_dates = request.get_json(force=True)
+    print(requested_dates)
+
+    response_dates = calculate_best_dates(requested_dates)
+
+    return jsonify(response_dates)
+
 def main():
-    pass
+    APP.run(debug=True)
 
 if __name__ == '__main__':
     main()
